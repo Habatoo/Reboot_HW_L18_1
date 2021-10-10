56ac007880dec7005f0b8ca78e34b7782a866593
habatoo <habatoo@gmail.com>
2021-10-10 17:47:08 +0700

    MusicBoxXmlImpl

--- src/main/java/laptenkov/MusicBox.java
+++ src/main/java/laptenkov/MusicBox.java
@@ -1,4 +1,23 @@
 package laptenkov;
 
+/**
+ * Класс {@link MusicBox} для воспроизведения музыки различных
+ * жанров. Музыка выбирается случайно внутри жанра.
+ * При начале проигрывания озвучиваются жанр, автор и название песни.
+ * @author habatoo
+ */
 public class MusicBox {
+    private Music music;
+
+    public MusicBox(Music music) {
+        this.music = music;
+    }
+
+    /**
+     *
+     */
+    public void play() {
+        System.out.println(music.play());
+    }
+
 }

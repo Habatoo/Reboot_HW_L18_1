682d7a609b1fd1b62ff8df48ec1b7ab934cac0c7
habatoo <habatoo@gmail.com>
2021-10-11 21:29:14 +0700

    fix

--- src/main/java/laptenkov/MusicBox.java
+++ src/main/java/laptenkov/MusicBox.java
@@ -1,23 +1,35 @@
 package laptenkov;
 
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Класс {@link MusicBox} для воспроизведения музыки различных
  * жанров. Музыка выбирается случайно внутри жанра.
  * При начале проигрывания озвучиваются жанр, автор и название песни.
+ *
  * @author habatoo
  */
 public class MusicBox {
-    private Music music;
+    private List<Music> musicList;
+    Random RANDOM = new Random();
 
-    public MusicBox(Music music) {
-        this.music = music;
+    public MusicBox(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     /**
-     *
+     * Метод {@link MusicBox#play()}
+     * воспроизводит музыку переданную в качестве параметра.
+     * Выход после ввода слова "quit".
      */
     public void play() {
-        System.out.println(music.play());
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.next().equals("quit")) {
+            List<Object> musicCompositions = musicList.get(RANDOM.nextInt(musicList.size())).play();
+            System.out.println(musicCompositions.get(RANDOM.nextInt(musicCompositions.size())));
+        }
     }
 
 }
